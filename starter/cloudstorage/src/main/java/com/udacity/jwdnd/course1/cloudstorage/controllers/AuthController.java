dfc4e5d44647fafaac066c5cbb6d6f430ed33f60
malshenaiber <malshenaiber@TAHAKOM.COM>
2023-01-06 20:08:47 +0300

    Implement authenticatiuon
    - Implement authentication service
    - Implement webSecurity Cofig
    - Update login / logout pages
    - Add login / logout controllers
    - Update server.port

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/AuthController.java
@@ -0,0 +1,24 @@
+package com.udacity.jwdnd.course1.cloudstorage.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AuthController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/signup")
+	public String signup() {
+		return "signup";
+	}
+
+	@GetMapping("/home")
+	public @ResponseBody String home() {
+		return "HOME!";
+	}
+}
