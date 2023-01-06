5a44f890f206e1ee1b914ffa5f67cb790e039da3
malshenaiber <malshenaiber@TAHAKOM.COM>
2023-01-06 20:25:01 +0300

    prepare sighup page

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/AuthController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/AuthController.java
@@ -2,23 +2,30 @@ package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AuthController {
 
 	@GetMapping("/login")
-	public String login() {
+	public String loginPage() {
 		return "login";
 	}
 
 	@GetMapping("/signup")
-	public String signup() {
+	public String signupPage() {
 		return "signup";
 	}
 
+	@PostMapping("/signup")
+	public String signupUser() {
+		// TODO: Add user credentials to the embedded database
+		return "login";
+	}
+
 	@GetMapping("/home")
-	public @ResponseBody String home() {
+	public @ResponseBody String homePage() {
 		return "HOME!";
 	}
 }
