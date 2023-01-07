aa8d80d645106994f95cfba6f08b85101988c8db
malshenaiber <malshenaiber@TAHAKOM.COM>
2023-01-08 00:27:55 +0300

    Code refactor

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/entities/File.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/entities/File.java
@@ -1,14 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage.entities;
 
-import java.sql.Blob;
-
 import lombok.Data;
 
 @Data
 public class File {
 	private Integer fileId;
-	private String filename;
-	private String contenttype;
-	private Integer filesize;
-	private Blob filedata;
+	private String fileName;
+	private String contentType;
+	private Integer fileSize;
+	private byte[] fileData;
 }
