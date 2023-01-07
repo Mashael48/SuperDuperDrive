fa68cc040196c2aae51a0940478400f8ab570f1c
malshenaiber <malshenaiber@TAHAKOM.COM>
2023-01-08 02:33:24 +0300

    Prepare result page for msgs view

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/utils/Constants.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/utils/Constants.java
@@ -4,6 +4,7 @@ public interface Constants {
 
 	// ** Pages ** //
 	public static final String HOME_PAGE = "home";
+	public static final String RESULT_PAGE = "result";
 
 	// ** Lists ** //
 	public static final String FILES_LIST = "filesList";
@@ -13,5 +14,14 @@ public interface Constants {
 	// ** FE variable ** //
 	public static final String FILE_FORM = "fileForm";
 	public static final String CREDENTIAL_SERVICE = "credentialService";
+
+	// ** Messages Status ** //
+	public static final String SUCCESS = "success";
+	public static final String NOT_SAVED = "notSaved";
+	public static final String ERROR = "error";
+
+	// ** Error messages ** //
+	public static final String MESSAGE = "message";
+	public static final String DUBLICATE_FILE_NAME = "File name is already in-use!";
 
 }
