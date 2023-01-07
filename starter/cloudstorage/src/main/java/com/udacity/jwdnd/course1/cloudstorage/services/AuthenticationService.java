877047a588db999917a5d0501ba7c4a81043ba1e
malshenaiber <malshenaiber@TAHAKOM.COM>
2023-01-07 17:58:18 +0300

    Implement user access management
    - Implement login validation
    - Implement sign-up

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -8,32 +8,40 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
+import com.udacity.jwdnd.course1.cloudstorage.entities.User;
+import com.udacity.jwdnd.course1.cloudstorage.mappers.UserMapper;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService implements AuthenticationProvider {
+
+	private final UserMapper userMapper;
+	private final HashService hashService;
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		// TODO: Add embedded database checking
+
 		String username = authentication.getName();
 		String password = authentication.getCredentials().toString();
 
-		log.info("usename: {}, password: {}", username, password);
-		if (username != null)
-			return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+		User user = userMapper.getUser(username);
+		if (user != null) {
+			log.info("User exist!");
 
+			String encodedSalt = user.getSalt();
+			String hashedPassword = hashService.getHashedValue(password, encodedSalt);
+
+			if (user.getPassword().equals(hashedPassword)) {
+				return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+			}
+		}
+
+		log.warn("User does not exist!");
 		return null;
-		// TODO: Add embedded database checking
-		/*
-		 * User user = userMapper.getUser(username); if (user != null) { String
-		 * encodedSalt = user.getSalt(); String hashedPassword =
-		 * hashService.getHashedValue(password, encodedSalt); if
-		 * (user.getPassword().equals(hashedPassword)) { return new
-		 * UsernamePasswordAuthenticationToken(username, password, new ArrayList<>()); }
-		 * }
-		 */
 
 	}
 
