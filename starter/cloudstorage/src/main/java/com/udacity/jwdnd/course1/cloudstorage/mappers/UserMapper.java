e825f193e483709e2ec0a3fe2e59bf538d8772ad
malshenaiber <malshenaiber@TAHAKOM.COM>
2023-01-07 18:26:05 +0300

    Code refactor
    - Remove getAllUsers code
    - Rename AuthController to UAM

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -1,7 +1,5 @@
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
@@ -18,9 +16,5 @@ public interface UserMapper {
 	@Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
 	@Options(useGeneratedKeys = true, keyProperty = "userId")
 	int insert(User user);
-
-	// TODO: Remove
-	@Select("SELECT * FROM USERS")
-	List<User> getAllUsers();
 
 }
