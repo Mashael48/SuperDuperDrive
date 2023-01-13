029bd264cce83df90855774b923b560efba5d33a
malshenaiber <malshenaiber@TAHAKOM.COM>
2023-01-13 21:35:43 +0300

    fix signupSuccess msg in login

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/UserAccessManagementController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/UserAccessManagementController.java
@@ -1,12 +1,13 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.udacity.jwdnd.course1.cloudstorage.entities.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
@@ -20,8 +21,16 @@ public class UserAccessManagementController {
 	private final UserService userService;
 
 	@GetMapping("/login")
-	public String loginPage(Model model, @ModelAttribute("flashAttribute") Object flashAttribute) {
-		model.addAttribute("signupSuccess", flashAttribute);
+	public String loginPage(Model model, HttpServletRequest request) {
+
+		// Get `signupSuccess` session value, then clear for next request
+		HttpSession session = request.getSession();
+		boolean signupSuccess = (session.getAttribute("signupSuccess") != null)
+				? (boolean) session.getAttribute("signupSuccess")
+				: false;
+		session.setAttribute("signupSuccess", false);
+
+		model.addAttribute("signupSuccess", signupSuccess);
 		return "login";
 	}
 
@@ -31,9 +40,10 @@ public class UserAccessManagementController {
 	}
 
 	@PostMapping("/signup")
-	public RedirectView signupUser(@ModelAttribute User user, Model model, RedirectAttributes attributes) {
+	public String signupUser(@ModelAttribute User user, Model model, HttpServletRequest request) {
 
 		String signupError = null;
+		HttpSession session = request.getSession(); // Get session if exist, otherwise create new one
 
 		if (!userService.isUsernameAvailable(user.getUsername())) {
 			signupError = "The username already exists.";
@@ -45,13 +55,14 @@ public class UserAccessManagementController {
 			}
 		}
 
-		if (signupError == null) {
-			attributes.addFlashAttribute("signupSuccess", true);
-		} else {
+		if (signupError != null) {
+			session.setAttribute("signupSuccess", false);
 			model.addAttribute("signupError", signupError);
+			return "signup";
 		}
 
-		return new RedirectView("login");
+		session.setAttribute("signupSuccess", true);
+		return "redirect:login"; // `redirect: to set the URL path for /login
 	}
 
 	@GetMapping("/logout")
